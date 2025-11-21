ec2ce3fdcde63f0a159a6e708cedd9a36989ef5f
PitriNurasiyah <fitriamrlia2001@gmail.com>
2025-11-21 23:21:50 +0700

    Delete Pertemuan 9/si_gudang directory

--- Pertemuan 9/si_gudang/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "si_gudang";
-
-// isi nama host, username mysql, dan password mysql anda
-$koneksi = mysqli_connect($host,$username,$password,$database);
-?> 
